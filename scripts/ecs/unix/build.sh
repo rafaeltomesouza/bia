05f923e191046a9ccf63bf8d1d32ed3dd5a4004d
Rafael Tomé de Souza <rafaeltomesouza@gmail.com>
2024-11-17 14:57:19 -0300

    Created file to control the codepipeline build

--- scripts/ecs/unix/build.sh
+++ scripts/ecs/unix/build.sh
@@ -1,5 +1,8 @@
 ECR_REGISTRY="SEU_REGISTRY"
+-- PRE_BUILD
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REGISTRY
+-- BUILD
 docker build -t bia .
 docker tag bia:latest $ECR_REGISTRY/bia:latest
+-- POST BUILD
 docker push $ECR_REGISTRY/bia:latest
